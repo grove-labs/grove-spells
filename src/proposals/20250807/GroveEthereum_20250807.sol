2873804d66ab66a4cbce5b4e00df2fdcc0f83499
kevinlichan <kevinlichan12@gmail.com>
2025-07-25 09:48:52 -0400

    feat: implemented onboardEthena

--- src/proposals/20250807/GroveEthereum_20250807.sol
+++ src/proposals/20250807/GroveEthereum_20250807.sol
@@ -6,11 +6,13 @@ import { GrovePayloadEthereum } from "src/libraries/GrovePayloadEthereum.sol";
 /**
  * @title  August 7, 2025 Grove Ethereum Proposal
  * @notice TODO
- * @author Steakhouse Financial
+ * @author Grove Labs
  * Forum : TODO
  * Vote  : TODO
  */
 contract GroveEthereum_20250807 is GrovePayloadEthereum {
+
+    address internal constant NEW_ALM_CONTROLLER = 0x28170D5084cc3cEbFC5f21f30DB076342716f30C;
 
     function _execute() internal override {
         _upgradeController();
@@ -47,7 +49,51 @@ contract GroveEthereum_20250807 is GrovePayloadEthereum {
     }
 
     function _onboardEthena() internal {
-        // TODO: Implement
+        // USDe mint/burn
+        RateLimitHelpers.setRateLimitData(
+            MainnetController(NEW_ALM_CONTROLLER).LIMIT_USDE_MINT(),
+            Ethereum.ALM_RATE_LIMITS,
+            IRateLimits.RateLimitData({
+                maxAmount : 250_000_000e6,
+                slope     : 100_000_000e6 / uint256(1 days)
+            }),
+            "ethenaMintLimit",
+            6
+        );
+        RateLimitHelpers.setRateLimitData(
+            MainnetController(NEW_ALM_CONTROLLER).LIMIT_USDE_BURN(),
+            Ethereum.ALM_RATE_LIMITS,
+            IRateLimits.RateLimitData({
+                maxAmount : 500_000_000e18,
+                slope     : 200_000_000e18 / uint256(1 days)
+            }),
+            "ethenaBurnLimit",
+            18
+        );
+
+        // sUSDe deposit (no need for withdrawal because of cooldown)
+        RateLimitHelpers.setRateLimitData(
+            RateLimitHelpers.makeAssetKey(
+                MainnetController(NEW_ALM_CONTROLLER).LIMIT_4626_DEPOSIT(),
+                Ethereum.SUSDE
+            ),
+            Ethereum.ALM_RATE_LIMITS,
+            IRateLimits.RateLimitData({
+                maxAmount : 250_000_000e18,
+                slope     : 100_000_000e18 / uint256(1 days)
+            }),
+            "susdeDepositLimit",
+            18
+        );
+
+        // Cooldown
+        RateLimitHelpers.setRateLimitData(
+            MainnetController(NEW_ALM_CONTROLLER).LIMIT_SUSDE_COOLDOWN(),
+            Ethereum.ALM_RATE_LIMITS,
+            RateLimitHelpers.unlimitedRateLimit(),
+            "susdeCooldownLimit",
+            18
+        );
     }
 
 }
--- src/proposals/20250807/GroveEthereum_20250807.t.sol
+++ src/proposals/20250807/GroveEthereum_20250807.t.sol
@@ -7,6 +7,8 @@ import "src/test-harness/GroveTestBase.sol";
 
 contract GroveEthereum_20250807Test is GroveTestBase {
 
+    address internal constant NEW_ALM_CONTROLLER = 0x28170D5084cc3cEbFC5f21f30DB076342716f30C;
+
     constructor() {
         id = "20250807";
     }
@@ -38,7 +40,7 @@ contract GroveEthereum_20250807Test is GroveTestBase {
         // TODO: Implement
     }
 
-    function test_ETHEREUM_onboardStakedUSDCVault() public onChain(ChainIdUtils.Ethereum()) {
+    function test_ETHEREUM_onboardSteakUSDCVault() public onChain(ChainIdUtils.Ethereum()) {
         vm.skip(true);
         // TODO: Implement
     }
@@ -49,8 +51,22 @@ contract GroveEthereum_20250807Test is GroveTestBase {
     }
 
     function test_ETHEREUM_onboardEthena() public onChain(ChainIdUtils.Ethereum()) {
-        vm.skip(true);
-        // TODO: Implement
+        executeAllPayloadsAndBridges();
+
+        MainnetController controller = MainnetController(NEW_ALM_CONTROLLER);
+
+        IERC20 usdc    = IERC20(Ethereum.USDC);
+        IERC20 usde    = IERC20(Ethereum.USDE);
+        IERC4626 susde = IERC4626(Ethereum.SUSDE);
+        
+        // Use realistic numbers to check the rate limits
+        uint256 usdcAmount = 5_000_000e6;
+        uint256 usdeAmount = usdcAmount * 1e12;
+
+        // Use deal2 for USDC because storage is not set in a common way
+        deal2(Ethereum.uSDC, Ethereum.ALM_PROXY, usdcAmount);
+
+        assertEq(usdc.allowance(Ethereum.ALM_PROXY, Ethereum.ETHENA_MINTER), 0);
     }
 
     function test_AVALANCHE_initializeLiquidityLayer() public onChain(ChainIdUtils.Avalanche()) {
