77e2c40540049864636547761f7503e4ee516559
Bartek Rutkowski <barrutko.eth@gmail.com>
2025-10-18 01:10:35 +0200

    refactor: minor improvements

--- src/proposals/20251030/GroveEthereum_20251030.sol
+++ src/proposals/20251030/GroveEthereum_20251030.sol
@@ -3,21 +3,16 @@ pragma solidity 0.8.25;
 
 import { Ethereum } from "lib/grove-address-registry/src/Ethereum.sol";
 
-import { MainnetController } from "grove-alm-controller/src/MainnetController.sol";
-import { RateLimitHelpers }  from "grove-alm-controller/src/RateLimitHelpers.sol";
-
-import { IRateLimits } from "grove-alm-controller/src/interfaces/IRateLimits.sol";
-
 import { GrovePayloadEthereum } from "src/libraries/GrovePayloadEthereum.sol";
 
 /**
  * @title  October 30, 2025 Grove Ethereum Proposal
  * @author Grove Labs
- * Forum : TODO Forum link
+ * Forum : https://forum.sky.money/t/october-30th-2025-proposed-changes-to-grove-for-upcoming-spell/27321
  */
 contract GroveEthereum_20251030 is GrovePayloadEthereum {
 
-    address internal constant CURVE_RLUSDUSDC           = 0xD001aE433f254283FeCE51d4ACcE8c53263aa186;
+    address internal constant CURVE_RLUSDUSDC = 0xD001aE433f254283FeCE51d4ACcE8c53263aa186;
 
     address internal constant AAVE_ATOKEN_CORE_USDC     = 0x98C23E9d8f34FEFb1B7BD6a91B7FF122F4e16F5c;
     address internal constant AAVE_ATOKEN_CORE_RLUSD    = 0xFa82580c16A31D0c1bC632A36F82e83EfEF3Eec0;
@@ -44,47 +39,48 @@ contract GroveEthereum_20251030 is GrovePayloadEthereum {
         // [Mainnet] Curve RLUSD/USDC Pool Onboarding
         //   Forum : https://forum.sky.money/t/october-30th-2025-proposed-changes-to-grove-for-upcoming-spell/27321
         _onboardCurvePool({
-            controller:    Ethereum.ALM_CONTROLLER,
-            pool:          CURVE_RLUSDUSDC,
-            maxSlippage:   CURVE_RLUSDUSDC_MAX_SLIPPAGE,
-            swapMax:       CURVE_RLUSDUSDC_SWAP_MAX,
-            swapSlope:     CURVE_RLUSDUSDC_SWAP_SLOPE,
-            depositMax:    0,
-            depositSlope:  0,
-            withdrawMax:   0,
-            withdrawSlope: 0
+            controller    : Ethereum.ALM_CONTROLLER,
+            pool          : CURVE_RLUSDUSDC,
+            maxSlippage   : CURVE_RLUSDUSDC_MAX_SLIPPAGE,
+            swapMax       : CURVE_RLUSDUSDC_SWAP_MAX,
+            swapSlope     : CURVE_RLUSDUSDC_SWAP_SLOPE,
+            depositMax    : 0,
+            depositSlope  : 0,
+            withdrawMax   : 0,
+            withdrawSlope : 0
         });
 
         // [Mainnet] Aave Core USDC Onboarding
         //   Forum : https://forum.sky.money/t/october-30th-2025-proposed-changes-to-grove-for-upcoming-spell/27321
-        _onboardAaveToken(
-            AAVE_ATOKEN_CORE_USDC,
-            AAVE_ATOKEN_CORE_USDC_DEPOSIT_MAX,
-            AAVE_ATOKEN_CORE_USDC_DEPOSIT_SLOPE
-        );
+        _onboardAaveToken({
+            token        : AAVE_ATOKEN_CORE_USDC,
+            depositMax   : AAVE_ATOKEN_CORE_USDC_DEPOSIT_MAX,
+            depositSlope : AAVE_ATOKEN_CORE_USDC_DEPOSIT_SLOPE
+        });
 
         // [Mainnet] Aave Core RLUSD Onboarding
         //   Forum : https://forum.sky.money/t/october-30th-2025-proposed-changes-to-grove-for-upcoming-spell/27321
-        _onboardAaveToken(
-            AAVE_ATOKEN_CORE_RLUSD,
-            AAVE_ATOKEN_CORE_RLUSD_DEPOSIT_MAX,
-            AAVE_ATOKEN_CORE_RLUSD_DEPOSIT_SLOPE
-        );
+        _onboardAaveToken({
+            token        : AAVE_ATOKEN_CORE_RLUSD,
+            depositMax   : AAVE_ATOKEN_CORE_RLUSD_DEPOSIT_MAX,
+            depositSlope : AAVE_ATOKEN_CORE_RLUSD_DEPOSIT_SLOPE
+        });
 
         // [Mainnet] Aave Horizon USDC Onboarding
         //   Forum : https://forum.sky.money/t/october-30th-2025-proposed-changes-to-grove-for-upcoming-spell/27321
-        _onboardAaveToken(
-            AAVE_ATOKEN_HORIZON_USDC,
-            AAVE_ATOKEN_HORIZON_USDC_DEPOSIT_MAX,
-            AAVE_ATOKEN_HORIZON_USDC_DEPOSIT_SLOPE
-        );
+        _onboardAaveToken({
+            token        : AAVE_ATOKEN_HORIZON_USDC,
+            depositMax   : AAVE_ATOKEN_HORIZON_USDC_DEPOSIT_MAX,
+            depositSlope : AAVE_ATOKEN_HORIZON_USDC_DEPOSIT_SLOPE
+        });
 
         // [Mainnet] Aave Horizon RLUSD Onboarding
         //   Forum : https://forum.sky.money/t/october-30th-2025-proposed-changes-to-grove-for-upcoming-spell/27321
-        _onboardAaveToken(
-            AAVE_ATOKEN_HORIZON_RLUSD,
-            AAVE_ATOKEN_HORIZON_RLUSD_DEPOSIT_MAX,
-            AAVE_ATOKEN_HORIZON_RLUSD_DEPOSIT_SLOPE
-        );
+        _onboardAaveToken({
+            token        : AAVE_ATOKEN_HORIZON_RLUSD,
+            depositMax   : AAVE_ATOKEN_HORIZON_RLUSD_DEPOSIT_MAX,
+            depositSlope : AAVE_ATOKEN_HORIZON_RLUSD_DEPOSIT_SLOPE
+        });
     }
+
 }
--- src/proposals/20251030/GroveEthereum_20251030.t.sol
+++ src/proposals/20251030/GroveEthereum_20251030.t.sol
@@ -1,26 +1,13 @@
 // SPDX-License-Identifier: AGPL-3.0
 pragma solidity 0.8.25;
 
-import { IERC20 } from "forge-std/interfaces/IERC20.sol";
-
-import { Ethereum } from "lib/grove-address-registry/src/Ethereum.sol";
-
 import { ChainIdUtils } from "src/libraries/ChainId.sol";
-
-import { MainnetController } from "grove-alm-controller/src/MainnetController.sol";
-import { RateLimitHelpers }  from "grove-alm-controller/src/RateLimitHelpers.sol";
-
-import { GroveLiquidityLayerContext } from "src/test-harness/GroveLiquidityLayerTests.sol";
 
 import { GroveTestBase } from "src/test-harness/GroveTestBase.sol";
 
-interface AutoLineLike {
-    function exec(bytes32) external;
-}
-
 contract GroveEthereum_20251030_Test is GroveTestBase {
 
-    address internal constant CURVE_RLUSDUSDC           = 0xD001aE433f254283FeCE51d4ACcE8c53263aa186;
+    address internal constant CURVE_RLUSDUSDC = 0xD001aE433f254283FeCE51d4ACcE8c53263aa186;
 
     address internal constant AAVE_ATOKEN_CORE_USDC     = 0x98C23E9d8f34FEFb1B7BD6a91B7FF122F4e16F5c;
     address internal constant AAVE_ATOKEN_CORE_RLUSD    = 0xFa82580c16A31D0c1bC632A36F82e83EfEF3Eec0;
@@ -60,53 +47,53 @@ contract GroveEthereum_20251030_Test is GroveTestBase {
 
     function test_ETHEREUM_curve_rlusdusdcOnboarding() public onChain(ChainIdUtils.Ethereum()) {
         _testCurveOnboarding({
-            pool:                        CURVE_RLUSDUSDC,
-            expectedDepositAmountToken0: 0,
-            expectedSwapAmountToken0:    EXPECTED_SWAP_AMOUNT_TOKEN0,
-            maxSlippage:                 CURVE_RLUSDUSDC_MAX_SLIPPAGE,
-            swapMax:                     CURVE_RLUSDUSDC_SWAP_MAX,
-            swapSlope:                   CURVE_RLUSDUSDC_SWAP_SLOPE,
-            depositMax:                  0,
-            depositSlope:                0,
-            withdrawMax:                 0,
-            withdrawSlope:               0
+            pool                        : CURVE_RLUSDUSDC,
+            expectedDepositAmountToken0 : 0,
+            expectedSwapAmountToken0    : EXPECTED_SWAP_AMOUNT_TOKEN0,
+            maxSlippage                 : CURVE_RLUSDUSDC_MAX_SLIPPAGE,
+            swapMax                     : CURVE_RLUSDUSDC_SWAP_MAX,
+            swapSlope                   : CURVE_RLUSDUSDC_SWAP_SLOPE,
+            depositMax                  : 0,
+            depositSlope                : 0,
+            withdrawMax                 : 0,
+            withdrawSlope               : 0
         });
     }
 
     function test_ETHEREUM_onboardAaveCoreUsdc() public onChain(ChainIdUtils.Ethereum()) {
-        _testAaveOnboarding(
-            AAVE_ATOKEN_CORE_USDC,
-            AAVE_ATOKEN_CORE_USDC_TEST_DEPOSIT,
-            AAVE_ATOKEN_CORE_USDC_DEPOSIT_MAX,
-            AAVE_ATOKEN_CORE_USDC_DEPOSIT_SLOPE
-        );
+        _testAaveOnboarding({
+            aToken                : AAVE_ATOKEN_CORE_USDC,
+            expectedDepositAmount : AAVE_ATOKEN_CORE_USDC_TEST_DEPOSIT,
+            depositMax            : AAVE_ATOKEN_CORE_USDC_DEPOSIT_MAX,
+            depositSlope          : AAVE_ATOKEN_CORE_USDC_DEPOSIT_SLOPE
+        });
     }
 
     function test_ETHEREUM_onboardAaveCoreRlusd() public onChain(ChainIdUtils.Ethereum()) {
-        _testAaveOnboarding(
-            AAVE_ATOKEN_CORE_RLUSD,
-            AAVE_ATOKEN_CORE_RLUSD_TEST_DEPOSIT,
-            AAVE_ATOKEN_CORE_RLUSD_DEPOSIT_MAX,
-            AAVE_ATOKEN_CORE_RLUSD_DEPOSIT_SLOPE
-        );
+        _testAaveOnboarding({
+            aToken                : AAVE_ATOKEN_CORE_RLUSD,
+            expectedDepositAmount : AAVE_ATOKEN_CORE_RLUSD_TEST_DEPOSIT,
+            depositMax            : AAVE_ATOKEN_CORE_RLUSD_DEPOSIT_MAX,
+            depositSlope          : AAVE_ATOKEN_CORE_RLUSD_DEPOSIT_SLOPE
+        });
     }
 
     function test_ETHEREUM_onboardAaveHorizonUsdc() public onChain(ChainIdUtils.Ethereum()) {
-        _testAaveOnboarding(
-            AAVE_ATOKEN_HORIZON_USDC,
-            AAVE_ATOKEN_HORIZON_USDC_TEST_DEPOSIT,
-            AAVE_ATOKEN_HORIZON_USDC_DEPOSIT_MAX,
-            AAVE_ATOKEN_HORIZON_USDC_DEPOSIT_SLOPE
-        );
+        _testAaveOnboarding({
+            aToken                : AAVE_ATOKEN_HORIZON_USDC,
+            expectedDepositAmount : AAVE_ATOKEN_HORIZON_USDC_TEST_DEPOSIT,
+            depositMax            : AAVE_ATOKEN_HORIZON_USDC_DEPOSIT_MAX,
+            depositSlope          : AAVE_ATOKEN_HORIZON_USDC_DEPOSIT_SLOPE
+        });
     }
 
     function test_ETHEREUM_onboardAaveHorizonRlusd() public onChain(ChainIdUtils.Ethereum()) {
-        _testAaveOnboarding(
-            AAVE_ATOKEN_HORIZON_RLUSD,
-            AAVE_ATOKEN_HORIZON_RLUSD_TEST_DEPOSIT,
-            AAVE_ATOKEN_HORIZON_RLUSD_DEPOSIT_MAX,
-            AAVE_ATOKEN_HORIZON_RLUSD_DEPOSIT_SLOPE
-        );
+        _testAaveOnboarding({
+            aToken                : AAVE_ATOKEN_HORIZON_RLUSD,
+            expectedDepositAmount : AAVE_ATOKEN_HORIZON_RLUSD_TEST_DEPOSIT,
+            depositMax            : AAVE_ATOKEN_HORIZON_RLUSD_DEPOSIT_MAX,
+            depositSlope          : AAVE_ATOKEN_HORIZON_RLUSD_DEPOSIT_SLOPE
+        });
     }
 
 }
