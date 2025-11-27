a964dd94d4a6e93f08d308456bdc20d019c705c7
Bartek Rutkowski <barrutko.eth@gmail.com>
2025-11-27 14:02:33 -0500

    refactor: remove Base spell

--- src/proposals/20251211/GroveEthereum_20251211.sol
+++ src/proposals/20251211/GroveEthereum_20251211.sol
@@ -2,16 +2,11 @@
 pragma solidity 0.8.25;
 
 import { Ethereum } from "lib/grove-address-registry/src/Ethereum.sol";
-import { Base }     from "lib/grove-address-registry/src/Base.sol";
 
 import { MainnetController } from "lib/grove-alm-controller/src/MainnetController.sol";
 import { RateLimitHelpers }  from "lib/grove-alm-controller/src/RateLimitHelpers.sol";
 
 import { IRateLimits } from "lib/grove-alm-controller/src/interfaces/IRateLimits.sol";
-
-import { CCTPForwarder } from "lib/xchain-helpers/src/forwarders/CCTPForwarder.sol";
-
-import { CastingHelpers } from "src/libraries/helpers/CastingHelpers.sol";
 
 import { GrovePayloadEthereum } from "src/libraries/payloads/GrovePayloadEthereum.sol";
 
@@ -44,11 +39,6 @@ contract GroveEthereum_20251211 is GrovePayloadEthereum {
     uint256 internal constant GROVE_X_STEAKHOUSE_USDC_MORPHO_VAULT_DEPOSIT_MAX   = 20_000_000e6;
     uint256 internal constant GROVE_X_STEAKHOUSE_USDC_MORPHO_VAULT_DEPOSIT_SLOPE = 20_000_000e6 / uint256(1 days);
 
-    // BEFORE :          0 max ;          0/day slope
-    // AFTER  : 50,000,000 max ; 50,000,000/day slope
-    uint256 internal constant CCTP_RATE_LIMIT_MAX   = 50_000_000e6;
-    uint256 internal constant CCTP_RATE_LIMIT_SLOPE = 50_000_000e6 / uint256(1 days);
-
     function _execute() internal override {
         // [Ethereum] Onboard Securitize Tokenized AAA CLO Fund (STAC)
         //   Forum : https://forum.sky.money/t/december-11th-2025-proposed-changes-to-grove-for-upcoming-spell/27459#p-104940-h-1-ethereum-onboard-securitize-tokenized-aaa-clo-fund-stac-2
@@ -61,10 +51,6 @@ contract GroveEthereum_20251211 is GrovePayloadEthereum {
         // [Ethereum] Onboard Morpho Grove x Steakhouse High Yield Vault USDC
         //   Forum : https://forum.sky.money/t/december-11th-2025-proposed-changes-to-grove-for-upcoming-spell/27459#p-104940-h-3-ethereum-onboard-morpho-grove-x-steakhouse-high-yield-vault-usdc-14
         _onboardGroveXSteakhouseUsdcMorphoVault();
-
-        // [Base] Onboard Grove Liquidity Layer and CCTP for Base
-        //   Forum : https://forum.sky.money/t/december-11th-2025-proposed-changes-to-grove-for-upcoming-spell/27459#p-104940-h-4-base-onboard-grove-liquidity-layer-and-cctp-for-base-20
-        _onboardCctpTransfersToBase();
     }
 
     function _onboardSecuritizeStac() internal {
@@ -109,21 +95,6 @@ contract GroveEthereum_20251211 is GrovePayloadEthereum {
             depositMax   : GROVE_X_STEAKHOUSE_USDC_MORPHO_VAULT_DEPOSIT_MAX,
             depositSlope : GROVE_X_STEAKHOUSE_USDC_MORPHO_VAULT_DEPOSIT_SLOPE
         });
-    }
-
-    function _onboardCctpTransfersToBase() internal {
-        MainnetController(Ethereum.ALM_CONTROLLER).setMintRecipient(
-            CCTPForwarder.DOMAIN_ID_CIRCLE_BASE,
-            CastingHelpers.addressToCctpRecipient(Base.ALM_PROXY)
-        );
-
-        // General key rate limit for all CCTP transfers was set in the GroveEthereum_20250807 proposal
-
-        bytes32 domainKey = RateLimitHelpers.makeDomainKey(
-            MainnetController(Ethereum.ALM_CONTROLLER).LIMIT_USDC_TO_DOMAIN(),
-            CCTPForwarder.DOMAIN_ID_CIRCLE_BASE
-        );
-        IRateLimits(Ethereum.ALM_RATE_LIMITS).setRateLimitData(domainKey, CCTP_RATE_LIMIT_MAX, CCTP_RATE_LIMIT_SLOPE);
     }
 
 }
