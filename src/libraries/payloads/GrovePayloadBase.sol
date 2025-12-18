f020496e391034a1f58f713d1a2bbf16f91c5ec8
Bartek Rutkowski <barrutko.eth@gmail.com>
2025-12-18 16:50:33 +0100

    feat: add GrovePayloadBase

---
+++ src/libraries/payloads/GrovePayloadBase.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.0;
+
+import { Base } from "lib/grove-address-registry/src/Base.sol";
+
+import { GroveLiquidityLayerHelpers } from "../helpers/GroveLiquidityLayerHelpers.sol";
+
+/**
+ * @dev    Base smart contract for Base.
+ * @author Steakhouse Financial
+ */
+abstract contract GrovePayloadBase {
+
+    function _onboardERC4626Vault(address vault, uint256 depositMax, uint256 depositSlope) internal {
+        GroveLiquidityLayerHelpers.onboardERC4626Vault(
+            Base.ALM_RATE_LIMITS,
+            vault,
+            depositMax,
+            depositSlope
+        );
+    }
+
+}
