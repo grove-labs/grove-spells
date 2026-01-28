a3adb068f721739365671e214fc3d3b81854847a
Bartek Rutkowski <barrutko.eth@gmail.com>
2026-01-28 21:07:25 +0100

    refactor: move address to a constant

--- src/proposals/20260212/GroveEthereum_20260212.sol
+++ src/proposals/20260212/GroveEthereum_20260212.sol
@@ -9,6 +9,8 @@ import { GrovePayloadEthereum } from "src/libraries/payloads/GrovePayloadEthereu
  */
 contract GroveEthereum_20260212 is GrovePayloadEthereum {
 
+    address internal constant GROVE_X_STEAKHOUSE_AUSD_MORPHO_VAULT = 0xBEEfF0d672ab7F5018dFB614c93981045D4aA98a;
+
     function _execute() internal override {
         // [Mainnet] Onboard Grove x Steakhouse AUSD Morpho Vault V2
         //   Forum : https://forum.sky.money/t/february-12-2026-proposed-changes-to-grove-for-upcoming-spell/27662#p-105571-h-1-mainnet-onboard-grove-x-steakhouse-ausd-morpho-vault-v2-2
@@ -17,11 +19,11 @@ contract GroveEthereum_20260212 is GrovePayloadEthereum {
 
     function _onboardGroveXSteakhouseAusdMorphoVault() internal {
         _onboardERC4626Vault({
-            vault             : 0xBEEfF0d672ab7F5018dFB614c93981045D4aA98a, // GROVE_X_STEAKHOUSE_AUSD_MORPHO_VAULT
-            depositMax        : 20_000_000e6,                               // BEFORE: 0
-            depositSlope      : 20_000_000e6 / uint256(1 days),             // BEFORE: 0
-            shareUnit         : 1e18,                                       // BEFORE: 0
-            maxAssetsPerShare : 2e6                                         // BEFORE: 0
+            vault             : GROVE_X_STEAKHOUSE_AUSD_MORPHO_VAULT,
+            depositMax        : 20_000_000e6,                   // BEFORE: 0
+            depositSlope      : 20_000_000e6 / uint256(1 days), // BEFORE: 0
+            shareUnit         : 1e18,                           // BEFORE: 0
+            maxAssetsPerShare : 2e6                             // BEFORE: 0
         });
     }
 
