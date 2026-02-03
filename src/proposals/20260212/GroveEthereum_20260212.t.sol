07ce54746f90154ffa97269e58c2377f5b4867a2
Bartek Rutkowski <barrutko.eth@gmail.com>
2026-02-03 13:08:59 +0100

    feat: deploy spell

--- src/proposals/20260212/GroveEthereum_20260212.t.sol
+++ src/proposals/20260212/GroveEthereum_20260212.t.sol
@@ -7,6 +7,8 @@ import { GroveTestBase } from "src/test-harness/GroveTestBase.sol";
 
 contract GroveEthereum_20260212_Test is GroveTestBase {
 
+    address internal constant ETHEREUM_PAYLOAD = 0xe045AA2065FDba35a0e0B5283e7f36a8ca96886a;
+
     address internal constant GROVE_X_STEAKHOUSE_AUSD_MORPHO_VAULT = 0xBEEfF0d672ab7F5018dFB614c93981045D4aA98a;
 
     constructor() {
@@ -14,9 +16,9 @@ contract GroveEthereum_20260212_Test is GroveTestBase {
     }
 
     function setUp() public {
-        setupDomains("2026-01-27T12:00:00Z");
+        setupDomains("2026-02-03T12:00:00Z");
 
-        deployPayloads();
+        chainData[ChainIdUtils.Ethereum()].payload = ETHEREUM_PAYLOAD;
     }
 
     function test_ETHEREUM_onboardGroveXSteakhouseAusdMorphoVault() public onChain(ChainIdUtils.Ethereum()) {
