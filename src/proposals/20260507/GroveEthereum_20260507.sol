3940f5e92bfe034f5e810cac4538656347697cc6
Bartek Rutkowski <barrutko.eth@gmail.com>
2026-04-24 14:32:06 +0200

    refactor: drop LZ receiver onboarding

--- src/proposals/20260507/GroveEthereum_20260507.sol
+++ src/proposals/20260507/GroveEthereum_20260507.sol
@@ -5,11 +5,6 @@ import { IERC20 } from "forge-std/interfaces/IERC20.sol";
 
 import { Ethereum } from "lib/grove-address-registry/src/Ethereum.sol";
 
-import { AllocatorBuffer } from "lib/dss-allocator/src/AllocatorBuffer.sol";
-import { AllocatorVault }  from "lib/dss-allocator/src/AllocatorVault.sol";
-
-import { LZForwarder } from "lib/xchain-helpers/src/forwarders/LZForwarder.sol";
-
 import { GrovePayloadEthereum } from "src/libraries/payloads/GrovePayloadEthereum.sol";
 
 /**
@@ -24,11 +19,6 @@ contract GroveEthereum_20260507 is GrovePayloadEthereum {
 
     uint256 internal constant GROVE_FOUNDATION_GRANT_AMOUNT = 800_000e18;
 
-    // TODO: Set Avalanche payload address before deployment
-    // constructor() {
-    //     PAYLOAD_AVALANCHE = address(0);
-    // }
-
     function _execute() internal override {
         // [Ethereum] Onboard Tokenized Treasury Instance — Initial Deposit (JTRSY)
         //   Forum : TODO: Add forum link
@@ -41,10 +31,6 @@ contract GroveEthereum_20260507 is GrovePayloadEthereum {
         // [Ethereum] Grove Treasury — Monthly Grant for Grove Foundation
         //   Forum : TODO: Add forum link
         _transferMonthlyGrantToGroveFoundation();
-
-        // [Ethereum + Avalanche] Upgrade Governance Relay to LayerZero V2
-        //   Forum : TODO: Add forum link
-        _configureLZGovernanceSender();
     }
 
     function _onboardTokenizedTreasuryInstanceJtrsy() internal {
@@ -58,19 +44,6 @@ contract GroveEthereum_20260507 is GrovePayloadEthereum {
             depositSlope      : 100_000_000e18 / uint256(1 days), // BEFORE: 0
             shareUnit         : 1e18,                             // BEFORE: 0
             maxAssetsPerShare : 3e18                              // BEFORE: 0
-        });
-    }
-
-    function _configureLZGovernanceSender() internal {
-        address[] memory dvns = new address[](2);
-        dvns[0] = LZForwarder.LAYER_ZERO_DVN_ETHEREUM;
-        dvns[1] = LZForwarder.NETHERMIND_DVN_ETHEREUM;
-
-        LZForwarder.configureSender({
-            endpoint  : LZForwarder.ENDPOINT_ETHEREUM,
-            remoteEid : LZForwarder.ENDPOINT_ID_AVALANCHE,
-            dvns      : dvns,
-            executor  : LZForwarder.EXECUTOR_ETHEREUM
         });
     }
 
--- src/proposals/20260507/GroveEthereum_20260507.t.sol
+++ src/proposals/20260507/GroveEthereum_20260507.t.sol
@@ -3,27 +3,11 @@ pragma solidity 0.8.25;
 
 import { IERC20 } from "forge-std/interfaces/IERC20.sol";
 
-import { Avalanche } from "lib/grove-address-registry/src/Avalanche.sol";
-import { Ethereum }  from "lib/grove-address-registry/src/Ethereum.sol";
+import { Ethereum } from "lib/grove-address-registry/src/Ethereum.sol";
 
-import { IExecutor } from "lib/grove-gov-relay/src/interfaces/IExecutor.sol";
-
-import { LZForwarder } from "lib/xchain-helpers/src/forwarders/LZForwarder.sol";
-import { LZReceiver }  from "lib/xchain-helpers/src/receivers/LZReceiver.sol";
-
-import { UlnConfig }      from "@layerzerolabs/lz-evm-messagelib-v2/contracts/uln/UlnBase.sol";
-import { ExecutorConfig } from "@layerzerolabs/lz-evm-messagelib-v2/contracts/SendLibBase.sol";
-
-import { ChainIdUtils }  from "src/libraries/helpers/ChainId.sol";
-import { CastingHelpers } from "src/libraries/helpers/CastingHelpers.sol";
+import { ChainIdUtils } from "src/libraries/helpers/ChainId.sol";
 
 import { GroveTestBase } from "src/test-harness/GroveTestBase.sol";
-
-interface ILayerZeroEndpointV2Like {
-    function getReceiveLibrary(address receiver, uint32 srcEid) external view returns (address lib, bool isDefault);
-    function getSendLibrary(address sender, uint32 dstEid) external view returns (address lib);
-    function getConfig(address oapp, address lib, uint32 eid, uint32 configType) external view returns (bytes memory config);
-}
 
 contract GroveEthereum_20260507_Test is GroveTestBase {
 
@@ -32,9 +16,6 @@ contract GroveEthereum_20260507_Test is GroveTestBase {
     address internal constant GROVE_FOUNDATION = 0xE3EC4CC359E68c9dCE15Bf667b1aD37Df54a5a42;
 
     uint256 internal constant GROVE_FOUNDATION_GRANT_AMOUNT = 800_000e18;
-
-    address internal constant AVALANCHE_OLD_CCTP_RECEIVER = 0x26e9512547feC1906C55256e491DfB6673D8C23f;
-    address internal constant AVALANCHE_NEW_LZ_RECEIVER   = 0x380Be2b91B63BF75B194913b6e2C07Df09598c22;
 
     constructor() {
         id = "20260507";
@@ -96,107 +77,6 @@ contract GroveEthereum_20260507_Test is GroveTestBase {
             groveProxyBalanceBefore - GROVE_FOUNDATION_GRANT_AMOUNT,
             "grove-proxy-balance-not-decreased"
         );
-    }
-
-    function test_AVALANCHE_upgradeGovernanceRelayToLayerZeroV2() public onChain(ChainIdUtils.Avalanche()) {
-        IExecutor  executor   = IExecutor(Avalanche.GROVE_EXECUTOR);
-        LZReceiver lzReceiver = LZReceiver(AVALANCHE_NEW_LZ_RECEIVER);
-
-        bytes32    submissionRole = executor.SUBMISSION_ROLE();
-
-        _verifyLayerZeroReceiverDeployment({
-            _executor : Avalanche.GROVE_EXECUTOR,
-            _receiver : AVALANCHE_NEW_LZ_RECEIVER
-        });
-
-        // Peer must be wired to the Ethereum Grove Proxy for srcEid = ENDPOINT_ID_ETHEREUM
-        assertEq(
-            lzReceiver.peers(LZForwarder.ENDPOINT_ID_ETHEREUM),
-            CastingHelpers.addressToLayerZeroRecipient(Ethereum.GROVE_PROXY),
-            "incorrect-peer"
-        );
-
-        // ULN (security stack) config must use the Nethermind + LayerZero DVNs for Avalanche
-        {
-            ILayerZeroEndpointV2Like lzEndpoint =
-                ILayerZeroEndpointV2Like(address(lzReceiver.endpoint()));
-
-            (address receiveLib, ) = lzEndpoint.getReceiveLibrary(AVALANCHE_NEW_LZ_RECEIVER, LZForwarder.ENDPOINT_ID_ETHEREUM);
-            assertEq(receiveLib, LZForwarder.RECEIVE_LIBRARY_AVALANCHE, "incorrect-receive-library");
-
-            UlnConfig memory cfg = abi.decode(
-                lzEndpoint.getConfig({
-                    oapp       : AVALANCHE_NEW_LZ_RECEIVER,
-                    lib        : receiveLib,
-                    eid        : LZForwarder.ENDPOINT_ID_ETHEREUM,
-                    configType : 2
-                }),
-                (UlnConfig)
-            );
-
-            assertEq(uint256(cfg.confirmations),        15, "incorrect-confirmations");
-            assertEq(uint256(cfg.requiredDVNCount),     2,  "incorrect-required-dvn-count");
-            assertEq(uint256(cfg.optionalDVNCount),     0,  "incorrect-optional-dvn-count");
-            assertEq(uint256(cfg.optionalDVNThreshold), 0,  "incorrect-optional-dvn-threshold");
-
-            // 0x962F… (LZ) < 0xa59B… (Nethermind) on Avalanche
-            assertEq(cfg.requiredDVNs[0], LZForwarder.LAYER_ZERO_DVN_AVALANCHE, "incorrect-required-dvn-0");
-            assertEq(cfg.requiredDVNs[1], LZForwarder.NETHERMIND_DVN_AVALANCHE,  "incorrect-required-dvn-1");
-        }
-
-        assertTrue (executor.hasRole(submissionRole, AVALANCHE_OLD_CCTP_RECEIVER));
-        assertFalse(executor.hasRole(submissionRole, AVALANCHE_NEW_LZ_RECEIVER));
-
-        executeAllPayloadsAndBridges();
-
-        assertTrue(executor.hasRole(submissionRole, AVALANCHE_OLD_CCTP_RECEIVER));
-        assertTrue(executor.hasRole(submissionRole, AVALANCHE_NEW_LZ_RECEIVER));
-    }
-
-    function test_ETHEREUM_configureLZGovernanceSender() public onChain(ChainIdUtils.Ethereum()) {
-        ILayerZeroEndpointV2Like endpoint =
-            ILayerZeroEndpointV2Like(LZForwarder.ENDPOINT_ETHEREUM);
-
-        address sender = Ethereum.GROVE_PROXY;
-        uint32  dstEid = LZForwarder.ENDPOINT_ID_AVALANCHE;
-
-        executeAllPayloadsAndBridges();
-
-        // --- Send library ---
-        address sendLib = endpoint.getSendLibrary(sender, dstEid);
-        assertTrue(sendLib != address(0), "send-library-not-configured");
-
-        // --- Executor config (configType = 1) ---
-        ExecutorConfig memory execCfg = abi.decode(
-            endpoint.getConfig({
-                oapp       : sender,
-                lib        : sendLib,
-                eid        : dstEid,
-                configType : 1
-            }),
-            (ExecutorConfig)
-        );
-
-        assertEq(execCfg.executor, LZForwarder.EXECUTOR_ETHEREUM, "incorrect-sender-executor");
-        assertGt(execCfg.maxMessageSize, 0, "sender-max-message-size-zero");
-
-        // --- ULN config (configType = 2) ---
-        UlnConfig memory ulnCfg = abi.decode(
-            endpoint.getConfig({
-                oapp       : sender,
-                lib        : sendLib,
-                eid        : dstEid,
-                configType : 2
-            }),
-            (UlnConfig)
-        );
-
-        assertEq(uint256(ulnCfg.confirmations), 15, "incorrect-sender-confirmations");
-        assertEq(uint256(ulnCfg.requiredDVNCount), 2, "incorrect-sender-required-dvn-count");
-
-        // 0x589d… (LZ) < 0xa59B… (Nethermind) on Ethereum
-        assertEq(ulnCfg.requiredDVNs[0], LZForwarder.LAYER_ZERO_DVN_ETHEREUM, "incorrect-sender-required-dvn-0");
-        assertEq(ulnCfg.requiredDVNs[1], LZForwarder.NETHERMIND_DVN_ETHEREUM,  "incorrect-sender-required-dvn-1");
     }
 
 }
